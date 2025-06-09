78510f73e8214aa588e38b53c699cb346a3bc278
Ramya <ramya.bala221190@gmail.com>
2025-06-09 22:33:09 +0530

    updated ports

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import express from 'express';
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send(`Welcome to Express application running in ${process.env.APP_ENV} on port ${process.env.PORT}`)
+    res.send(`Welcome to Express application running in ${process.env.APP_ENV} on port ${process.env.APP_PORT}`)
 })
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server listening on ${process.env.PORT}`)
+app.listen(process.env.APP_PORT,()=>{
+    console.log(`server listening on ${process.env.APP_PORT}`)
 })
